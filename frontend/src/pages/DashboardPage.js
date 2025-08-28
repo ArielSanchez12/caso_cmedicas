5e7dd58e03e87f4dc77f7afdfdb2121f4b07edb2
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-08-28 09:27:32 -0500

    cambie color

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -12,7 +12,7 @@ function DashboardPage() {
   };
 
   return (
-    <div style={{ maxWidth: 400, margin: 'auto', padding: 20, borderRadius: 10, background: 'linear-gradient(135deg, #f9fafc 60%, #ffe5b4 100%)', boxShadow: '0 4px 24px rgba(0,0,0,0.10)', position: 'relative'}}>
+    <div style={{ maxWidth: 400, margin: 'auto', padding: 20, borderRadius: 10, background: 'linear-gradient(135deg, #f9fafc 60%, #ff6aebff 100%)', boxShadow: '0 4px 24px rgba(0,0,0,0.10)', position: 'relative'}}>
       <h2>Bienvenido - {usuario?.nombre}</h2>
       <h3>Seleccione un módulo:</h3>
       <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
